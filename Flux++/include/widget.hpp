8c0ba38a4c93d134615bc387d29295c3f5daefd4
glinren <glinren.kre@gmail.com>
2021-03-07 14:42:13 +0100

    added some gui_events

--- Flux++/include/widget.hpp
+++ Flux++/include/widget.hpp
@@ -1,48 +1,69 @@
 #ifndef WIDGET_HPP
 #define WIDGET_HPP
-
-#include <mem_comparable_closure.hpp>
 #include <string>
 #include <tuple>
 #include <array>
+#include <mem_comparable_closure.hpp>
+#include "gui_event.hpp"
 
 namespace fluxpp{ 
-  /* 
+  
  namespace widget{
-    using std::tuple;
-    using std::array;
-    using transparent_closure::Fun;
+   using mem_comparable_closure::Fun;
+   using std::tuple;
+   using std::array;
+   
+   struct AppEvent{
+     std::string target;  
+   };
+   
 
-    struct BaseAppEvent{
-      std::string target;  
-    };
-    
-    struct GuiEvent{};
-    struct WidgetReturnType{};
-    
-    template<class return_t>
-    struct Filter {
-      std::string target;
-      return_t (*fn)(void*); 
-    };
+   
+   struct WidgetReturnType{};
+   
+   template<class return_t>
+   struct Filter {
+     std::string target;
+     return_t (*fn)(void*); 
+   };
+   
+   template<class app_event_t, class gui_event_t>
+   struct EventHandler {
+     Fun<app_event_t,gui_event_t> function;
+   };
+   
+   enum class WidgetClass{
+     application,
+     display,
+     window_visual_2d,
+     widget_visual_2d,
+     widget_leaf_visual_2d,
+     none
+   };
 
-    template<class app_event_t>
-    struct EventHandler {
-      Fun<app_event_t,GuiEvent> function;
-    };
+   template<WidgetClass widget_class>
+   struct get_render_return_type;
 
+   // //   template<>
+   // struct get_render_return_type
+   
+   template<WidgetClass widget_class_, class ... Ts>
+   class Widget;
 
-    
-    template<class app_event_t, int napp_handlers, class ... state_ts>
-    class Widget{
-    private:
-      tuple<Filter<state_ts>...> filters ;
-      Fun< WidgetReturnType, state_ts...> render;
-      array<EventHandler<app_event_t>, napp_handlers> app_handlers;
-    };
-    // Widget( filters, renderfn, event_handlers);
-}// widget
-*/
+   
+   template<WidgetClass widget_class_,  class app_event_t, class ... event_ts , class ... state_ts>
+   class Widget<widget_class_, tuple<EventHandler<app_event_t, event_ts>...>, state_ts...>{
+   public:
+     using render_return_type = typename get_render_return_type<widget_class_>::type;
+   private:
+     tuple<Filter<state_ts>...> filters ;
+     Fun< render_return_type, state_ts...> render;
+     tuple<EventHandler<app_event_t, event_ts>...> event_handlers;
+   };
+
+   
+   // Widget( filters, renderfn, event_handlers);
+ }// widget
   
 } // fluxpp
 
---
+++ Flux++/test/gui_event.cpp
@@ -0,0 +1,10 @@
+#include <doctest/doctest.h>
+
+#include "gui_event.hpp"
+
+
+
+TEST_CASE("something"){
+  CHECK(true);
+
+};
